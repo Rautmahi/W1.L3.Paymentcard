f3b7a912b081a47ab3fc4b37cac1cfcdcb012980
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2022-05-29 11:49:14 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from"./components/Card"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+  
+      
+      
+      <Card date={"17 Sep 2021 "} case={"Case Study"} h1={"APPLE Gift"} h2={"Payment"} h3={"MacOS-Mobile"} url={ "https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white.png"}/>
+      
+      
+      
+      
+      
+      
+      
     </div>
   );
 }
